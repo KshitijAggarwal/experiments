70aa95b14c9894d954e5fc0c7aec37917f55233f
KshitijAggarwal <kshitijagg13@gmail.com>
2024-06-12 16:11:03 -0500

    adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="experiments",
+    version="0.1.0",
+    description="Experiments library",
+    author="Kshitij Aggarwal",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies required by your library
+    ],
+)
