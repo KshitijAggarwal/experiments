d29725934ac1509b13b43804dd482b9fc48e2fcf
KshitijAggarwal <kshitijagg13@gmail.com>
2024-06-14 20:36:39 -0500

    adding lr

--- lm/dev/training_prep.py
+++ lm/dev/training_prep.py
@@ -39,8 +39,7 @@ x = buf[:-1].view(B, T)
 y = buf[1:].view(B, T)
 
 model.to(device)
-# RuntimeError: Dynamo is not supported on Python 3.12+
-# model = torch.compile(model)
+# model = torch.compile(model) # RuntimeError: Dynamo is not supported on Python 3.12+
 
 # %%
 
@@ -84,6 +83,7 @@ model.to(device)
 # dataloader
 
 import tiktoken
+import math
 
 
 class DataLoaderLite:
@@ -119,17 +119,44 @@ class DataLoaderLite:
 
 # %%
 
+
+def get_lr(step, warmup_steps, max_steps, max_lr, min_lr):
+    # warmup + cosine decay LR schedule
+    # 1) Linear warmup
+    if step < warmup_steps:
+        return max_lr * (step + 1) / warmup_steps
+
+    # 2) if step > max_steps, return min_lr
+    if step > max_steps:
+        return min_lr
+
+    # 3) cosine decay in between the above two
+    progress = (step - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= progress <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * progress))
+    return min_lr + coeff * (max_lr - min_lr)
+
+
+# %%
+
 import time
 
-B, T = 16, 128
+B, T = 16, 64
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
 train_loader = DataLoaderLite(B, T)
 
 # training loop
-nsteps = 50
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+max_steps = 50
+# optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimizer(
+    weight_decay=0.1, learning_rate=6e-4, device=device
+)
 
 model.train()
-for i in range(nsteps):
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -138,9 +165,22 @@ for i in range(nsteps):
     logits = model(x.to(device))
     loss = ce(logits.view(-1, logits.shape[-1]), y.view(-1))
     loss.backward()
+
+    # gradient clipping, GPT-3 paper.
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+    # determine and set the learning rate for this iteration
+    lr = get_lr(step, warmup_steps, max_steps, max_lr, min_lr)
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
     optimizer.step()
     t1 = time.time()
-    print(f"step: {i}, loss: {loss.item()}, dt: {t1-t0}")
+    dt = t1 - t0
+    tokens_per_sec = train_loader.B * train_loader.T / dt
+    print(
+        f"step {step:2d}, loss: {loss.item():.6f}, lr: {lr:.4e}, norm: {norm:.4f}, dt: {dt*1000:.2f}ms, tok/sec: {tokens_per_sec:.2f}"
+    )
 
 # %%
 
