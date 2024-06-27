651ad52ed6e1f01de8f423173e92fdbb83299a08
KshitijAggarwal <kshitijagg13@gmail.com>
2024-06-27 04:57:05 +0000

    cleanup

--- lm/dev/plot_logs.py
+++ lm/dev/plot_logs.py
@@ -1,3 +1,4 @@
+# %% 
 import pylab as plt
 
 def read_log_file(file_path):
@@ -19,24 +20,31 @@ def read_log_file(file_path):
     
     return train_data, val_data
 
-def plot_data(train_data, val_data):
+def plot_data(train_data, val_data, label):
     train_iterations, train_values = zip(*train_data)
     val_iterations, val_values = zip(*val_data)
     
-    plt.figure(figsize=(10, 6))
-    plt.plot(train_iterations, train_values, label='Train')
-    plt.plot(val_iterations, val_values, label='Validation')
+    # plt.figure(figsize=(10, 6))
+    plt.plot(train_iterations, train_values, label=f'Train_{label}')
+    plt.plot(val_iterations, val_values, label=f'Validation_{label}')
     
-    plt.xlabel('Iteration')
-    plt.ylabel('Value')
-    plt.title('Training and Validation Data')
+    plt.xlabel('Steps')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Losses')
     plt.legend()
     plt.grid(True)
-    
-    plt.show()
+    plt.ylim([3.5, 5])
+    # plt.show()
+
+# %% 
 
 # Usage
 file_path = '/root/experiments/lm/log/log_interp_29M.txt'
 train_data, val_data = read_log_file(file_path)
-plot_data(train_data, val_data)
+plot_data(train_data, val_data, '29M')
 
+file_path = '/root/experiments/lm/log/log_interp_17M.txt'
+train_data, val_data = read_log_file(file_path)
+plot_data(train_data, val_data, '17M')
+
+# %% 
--- lm/infer.py
+++ lm/infer.py
@@ -1,64 +1,31 @@
 # %%
+
 import torch
 import os
 from lm.utils import gen_text, GPTConfig
 
-filename = (
-    "/Users/kshitijaggarwal/Documents/Projects/experiments/lm/log/model_GPT2_00009.pt"
-)
+def remove_orig_mod(state_dict):
+    return {k.replace('_orig_mod.', ''): v for k, v in state_dict.items()}
+
+filename = '/workspace/log/model_GPT_19073_interp_29M.pt'
+# filename = '/workspace/log/model_GPT_04000_interp_17M.pt'
+
 
 if not os.path.isfile(filename):
     raise FileNotFoundError(f"No checkpoint found at {filename}")
 
 checkpoint = torch.load(filename)
 
-
-def load_checkpoint(checkpoint, model, optimizer, **kwargs):
-
-    model.load_state_dict(checkpoint["model_state_dict"])
-    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-
-    # Load scheduler state if it exists and a scheduler is provided
-    if checkpoint["scheduler_state_dict"] and kwargs.get("scheduler"):
-        kwargs["scheduler"].load_state_dict(checkpoint["scheduler_state_dict"])
-
-    # Set random states if needed
-    if kwargs.get("set_rng_state", False):
-        torch.set_rng_state(checkpoint["torch_rng_state"])
-
-    print(f"Checkpoint loaded from {filename}")
-    return model, optimizer
-
-
-# %%
-
 config = GPTConfig(**checkpoint["model_config"])
-if checkpoint["model_name"] == "GPT2":
-    from lm.gpt2 import GPT2
-
-    model = GPT2(config)
-else:
-    from lm.bitnet import BitNet
-
-    model = BitNet(config)
-
-optimizer = model.configure_optimizers(
-    weight_decay=0.01, learning_rate=6e-4, device_type="cpu"
-)
+from lm.gpt2 import GPT2
+model = GPT2(config)
 
 # %%
 
-# optimizer = torch.optim.AdamW(model.parameters())
-model, optimizer = load_checkpoint(
-    checkpoint,
-    model,
-    optimizer,
-    set_rng_state=True,
-)
-
-# %%
-
+model.load_state_dict(remove_orig_mod(checkpoint["model_state_dict"]))
+model.to('cuda')
 max_length = 32
-gen_text(model, 5, max_length, "cpu")
+gen_text(model, 5, max_length, "cuda", 
+         prefix="Hi, I am a helpful AI assistant. ")
 
 # %%
